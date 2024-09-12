2a706df27d8c77901310ac6fec924b21d10c79a7
Oksana Danilova <ilimlaw38@gmail.com>
2024-09-12 18:46:18 +0800

    Изменения внесены

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,29 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const dropdownValue = document.querySelector('.dropdown__value');
 	const dropdownList = document.querySelector('.dropdown__list');
-	const dropdownItems = document.querySelectorAll('.dropdown__item');
-	const dropdownLink = document.querySelectorAll('.dropdown__link')
-	const dropdownLinks = document.querySelectorAll('.dropdown__link');
+	const dropdownLinks = Array.from(document.querySelectorAll('.dropdown__link'));
 
-	dropdownLinks.forEach(link => {
-		link.addEventListener('click', (event) => {
-			event.preventDefault();
-		});
+	dropdownValue.addEventListener('click', function() {
+		dropdownList.classList.toogle('dropdown__list_active');
 	});
 
-	dropdownValue.onclick = () => {
-
-		dropdownList.classList.add('dropdown__list_active');
-
-	}
-
-	dropdownItems.forEach(function(dropdownItem) {
-		dropdownItem.onclick = () => {
+	dropdownLinks.forEach(element => {
+		element.addEventListener('click', function(event) {
+			event.preventDefault();
 			dropdownList.classList.remove('dropdown__list_active');
-			dropdownValue.textContent = dropdownItem.textContent;
-
-		}
-
-	})
-
+			dropdownValue.textContent = event.currentTarget.textContent;
+		});
+	});
 });
