80b4bed3b4fbdeb4b4f0aa5cf8ed50f8deef2031
Oksana Danilova <ilimlaw38@gmail.com>
2024-09-27 19:44:36 +0800

    Текстовый редактор

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,18 @@
+const editor = document.getElementById('editor');
+
+if (localStorage.getItem('text')) {
+    editor.value = localStorage.getItem('text');
+}
+
+editor.addEventListener('input', function() {
+    localStorage.setItem('text', editor.value);
+});
+
+const clearButton = document.createElement('button');
+clearButton.textContent = 'Очистить содержимое';
+document.body.appendChild(clearButton);
+
+clearButton.addEventListener('click', function() {
+    editor.value = '';
+    localStorage.removeItem('text');
+});
